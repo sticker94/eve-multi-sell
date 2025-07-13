e4e0f7c3d7291270edbb859ac4bbf8975a34ad3f
Nicholas Dowell <6182488+sticker94@users.noreply.github.com>
2025-07-12 21:32:51 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="eve-multi-sell",
     version="0.1.0",
-    py_modules=["eve_multi_sell"],        # rename your script eve_multi_sell.py
+    py_modules=["eve_multi_sell"],
     install_requires=["requests", "pyperclip"],
     entry_points={
         "console_scripts": [
